ebb1bb9de4c7192b0a51dd390b8bc95547bbb634
John Pham <johnphammail@gmail.com>
2020-12-05 21:17:16 -0800

    Clean up part 1

--- day6/index.js
+++ day6/index.js
@@ -4,53 +4,45 @@ const main = async () => {
   const rawInput = await f("https://adventofcode.com/2020/day/6/input");
   const input = rawInput.split("\n\n");
 
-  const allAnswers = [];
+  const allAnswers = input.map((group) => {
+    const groupAnswers = new Set();
+    group.split("\n").forEach((person) => {
+      [...person].forEach((answer) => groupAnswers.add(answer));
+    });
+    return groupAnswers.size;
+  });
+  console.log(allAnswers.reduce((acc, cur) => acc + cur, 0));
+
   // input.forEach((group) => {
-  //   // console.log(group, "\n");
-  //   const groupAnswers = new Set();
+  //   const groupAnswers = {};
+
   //   group.split("\n").forEach((person) => {
-  //     [...person].forEach((answer) => groupAnswers.add(answer));
+  //     [...person].forEach((answer) => {
+  //       if (groupAnswers[answer]) {
+  //         groupAnswers[answer] += 1;
+  //       } else {
+  //         groupAnswers[answer] = 1;
+  //       }
+  //     });
   //   });
-  //   allAnswers.push(groupAnswers.size);
-  //   // console.log(groupAnswers.size);
+
+  //   const numInGroup = group.split("\n").length;
+  //   const keys = Object.keys(groupAnswers);
+
+  //   keys.forEach((key) => {
+  //     if (groupAnswers[key] !== numInGroup) {
+  //       delete groupAnswers[key];
+  //     }
+  //   });
+  //   allAnswers.push(Object.keys(groupAnswers).length);
   // });
+
   // let total = 0;
   // // console.log(allAnswers);
   // allAnswers.forEach((answer) => {
   //   total += answer;
   // });
   // console.log({ total });
-
-  input.forEach((group) => {
-    const groupAnswers = {};
-
-    group.split("\n").forEach((person) => {
-      [...person].forEach((answer) => {
-        if (groupAnswers[answer]) {
-          groupAnswers[answer] += 1;
-        } else {
-          groupAnswers[answer] = 1;
-        }
-      });
-    });
-
-    const numInGroup = group.split("\n").length;
-    const keys = Object.keys(groupAnswers);
-
-    keys.forEach((key) => {
-      if (groupAnswers[key] !== numInGroup) {
-        delete groupAnswers[key];
-      }
-    });
-    allAnswers.push(Object.keys(groupAnswers).length);
-  });
-
-  let total = 0;
-  // console.log(allAnswers);
-  allAnswers.forEach((answer) => {
-    total += answer;
-  });
-  console.log({ total });
 };
 
 main();
